9120f76ab034c0b1c5b7e75ec055ba41d735ad74
kiran-bsv <boddepallisai@iitbhilai.ac.in>
2023-09-14 03:09:51 +0530

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { HashLoader } from 'react-spinners';
 import { usePostsContext } from '../hooks/usePostsContext';
 import { useAuthContext } from '../hooks/useAuthContext.js';
 import PostHead from '../components/PostHead';
@@ -26,6 +27,17 @@ const Home = () => {
 
         if (user) fetchPosts();
     }, [user, dispatch]);
+
+    if (!posts) {
+        return (
+            <div className="spinner">
+                <HashLoader
+                    color="#36d7b7"
+                    size={100}
+                />
+            </div>
+        );
+    }
 
     return(
         <>
